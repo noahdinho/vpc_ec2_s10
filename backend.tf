aafe03b2422f2733715bca51beac82d5c9e0d928
noahdinho <touagues@yahoo.com>
2024-08-08 03:01:03 -0500

    terraform_lb_s10

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 
 terraform {
   backend "s3" {
-    bucket  = "w7-sl-terraform"
+    bucket  = "perso"
     key     = "session10/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
-    //dynamodb_table = "locktable"
+    //dynamodb_table = "perso1"
   }
 }
 
